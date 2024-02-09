fde84e6116966b9c8a4415a8790df12890019dad
Pedro Fontoura <me@pedrofontoura.io>
2024-02-09 03:40:56 -0300

    feat: hook breaking changes
    The old implementation was wrapping too much functionality from Colyseus. The ONLY goal of this hook is to facilitate the use of Colyseus state inside React.
    
    I'll write a technical documentation explaining the decisions and how the hook works at some point. If you can't wait, it's heavily inspired by MobX. See https://mobx.js.org/understanding-reactivity.html

--- packages/use-colyseus/src/colyseus.ts
+++ packages/use-colyseus/src/colyseus.ts
@@ -1,134 +1,51 @@
 import { Schema } from "@colyseus/schema";
-import { Client, type Room } from "colyseus.js";
+import { type Room } from "colyseus.js";
 import { useSyncExternalStore } from "react";
 
-import { store } from "./store";
+import { proxy } from "./proxy";
 
-export const colyseus = <S = Schema>(
-  endpoint: string,
-  schema?: new (...args: unknown[]) => S
-) => {
-  const client = new Client(endpoint);
-
-  const roomStore = store<Room<S> | undefined>(undefined);
-  const stateStore = store<S | undefined>(undefined);
-
-  let connecting = false;
-
-  const connectToColyseus = async (roomName: string, options = {}) => {
-    if (connecting || roomStore.get()) return;
-
-    connecting = true;
-
-    try {
-      const room = await client.joinOrCreate<S>(roomName, options, schema);
-
-      roomStore.set(room);
-      stateStore.set(room.state);
-
-      const updatedCollectionsMap: { [key in keyof S]?: boolean } = {};
-
-      for (const [key, value] of Object.entries(room.state as Schema)) {
-        if (
-          typeof value !== "object" ||
-          !value.clone ||
-          !value.onAdd ||
-          !value.onRemove
-        ) {
-          continue;
-        }
-
-        updatedCollectionsMap[key as keyof S] = false;
-
-        value.onAdd(() => {
-          updatedCollectionsMap[key as keyof S] = true;
-        });
-
-        value.onRemove(() => {
-          updatedCollectionsMap[key as keyof S] = true;
-        });
-      }
-
-      room.onStateChange((state) => {
-        if (!state) return;
-
-        const copy = { ...state };
-
-        for (const [key, update] of Object.entries(updatedCollectionsMap)) {
-          if (!update) continue;
-
-          updatedCollectionsMap[key as keyof S] = false;
-
-          const value = state[key as keyof S] as unknown;
-
-          if ((value as Schema).clone) {
-            //@ts-ignore
-            copy[key as keyof S] = value.clone();
-          }
-        }
-
-        stateStore.set(copy);
-      });
-
-      console.log(
-        `Succesfully connected to Colyseus room ${roomName} at ${endpoint}`
-      );
-    } catch (e) {
-      console.error("Failed to connect to Colyseus!");
-      console.log(e);
-    } finally {
-      connecting = false;
-    }
-  };
-
-  const disconnectFromColyseus = async () => {
-    const room = roomStore.get();
-    if (!room) return;
-
-    roomStore.set(undefined);
-    stateStore.set(undefined);
-
-    try {
-      await room.leave();
-      console.log("Disconnected from Colyseus!");
-    } catch {}
-  };
-
-  const useColyseusRoom = () => {
-    const subscribe = (callback: () => void) =>
-      roomStore.subscribe(() => callback());
-
-    const getSnapshot = () => {
-      const colyseus = roomStore.get();
-      return colyseus;
-    };
-
-    return useSyncExternalStore(subscribe, getSnapshot);
-  };
-
+export const colyseus = <S extends Schema>(room: Room<S>) => {
   function useColyseusState(): S | undefined;
   function useColyseusState<T extends (state: S) => unknown>(
     selector: T
   ): ReturnType<T> | undefined;
   function useColyseusState<T extends (state: S) => unknown>(selector?: T) {
-    const subscribe = (callback: () => void) =>
-      stateStore.subscribe(() => {
-        callback();
-      });
+    const path: string[] = [];
 
-    const getSnapshot = () => {
-      const state = stateStore.get();
-      return state && selector ? selector(state) : state;
+    if (selector) {
+      // NOTE(pedr0fontoura): .toJSON works like a charm here, because we just need to know the path to the property the user is accessing
+      selector(proxy(room.state.toJSON(), path) as S);
+    }
+
+    if (path.length < 1) {
+      const subscribe = (callback: () => void) =>
+        room.state.onChange(() => callback());
+      const getSnapshot = () => room.state.clone();
+      return useSyncExternalStore(subscribe, getSnapshot);
+    }
+
+    // TODO(pedr0fontoura): Attach the appropriate listener(s) depending on the accessed property
+    let value: any = room.state;
+
+    for (let i = 0; i < path.length; i++) {
+      // If this is the last accessed property
+      if (i === path.length - 1) {
+      }
+
+      value = value[path[i]];
+    }
+
+    const subscribe = (callback: () => void) => {
+      callback();
+      return () => {};
     };
+
+    const getSnapshot = () => {};
 
     return useSyncExternalStore(subscribe, getSnapshot);
   }
 
   return {
-    client,
-    connectToColyseus,
-    disconnectFromColyseus,
-    useColyseusRoom,
     useColyseusState,
   };
 };
