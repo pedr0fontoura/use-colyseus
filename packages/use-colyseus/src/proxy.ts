bf49c6ef354fab9e1b9bba8ab1eef73bf0d8ae92
Pedro Fontoura <me@pedrofontoura.io>
2024-02-09 03:36:33 -0300

    feat: add proxy to track dereferenced properties

---
+++ packages/use-colyseus/src/proxy.ts
@@ -0,0 +1,35 @@
+export const proxy = (target: object, path: string[]) => {
+  const handler: ProxyHandler<{ [key: string]: unknown }> = {
+    get(target, key) {
+      // Safety check
+      if (key == "__isUseColyseusProxy") return true;
+
+      const value = target[key as string];
+
+      // We just want the enumerable properties
+      const keys = Object.keys(target);
+      if (!keys.includes(key as string)) return value;
+
+      if (value === null) return null;
+      if (typeof value == "undefined") return;
+
+      // At this point the accessed property is valid and we should track it
+      path.push(key as string);
+
+      if (
+        typeof value === "object" &&
+        !(value as { __isUseColyseusProxy: boolean }).__isUseColyseusProxy
+      ) {
+        target[key as string] = new Proxy(value, handler);
+      }
+
+      return target[key as string];
+    },
+    set(target, key, value) {
+      target[key as string] = value;
+      return true;
+    },
+  };
+
+  return new Proxy(target, handler);
+};
